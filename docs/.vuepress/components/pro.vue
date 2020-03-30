434dec5492cb8598308591867ebcec56b9a922cd
Bailey Matthews <baileyjmatthews@gmail.com>
2020-03-30 20:38:57 +0100

    Updated pro badge

--- docs/.vuepress/components/pro.vue
+++ docs/.vuepress/components/pro.vue
@@ -1,8 +1,15 @@
 <template>
-<svg viewBox="0 0 35 20" height="20px" xmlns="http://www.w3.org/2000/svg"><g shape-rendering="crispEdges"><path d="M0 0h33v20H0z" fill="#555"/></g><g fill="#fff" font-size="110" text-anchor="middle"><text transform="scale(.1)" x="165" y="140" textLength="230">PRO</text></g></svg> 
+  <svg viewBox="0 0 35 20" height=".75em" xmlns="http://www.w3.org/2000/svg">
+    <title>This feature is only available for projects with a professional subscription.</title>
+    <g shape-rendering="crispEdges"><path d="M0 0h33v20H0z" fill="#555" /></g>
+    <g fill="#fff" font-size="110" text-anchor="middle">
+      <text transform="scale(.1)" x="165" y="140" textLength="230">PRO</text>
+    </g>
+  </svg>
 </template>
+
 <script>
 export default {
-  name: 'pro'
-};</script>
-
+  name: "pro"
+};
+</script>
