27f5ce3ebea77e6d9c883bead88182c5498ea85d
Bailey Matthews <baileyjmatthews@gmail.com>
2020-03-30 19:54:08 +0100

    Added "pro" badge

---
+++ docs/.vuepress/components/pro.vue
@@ -0,0 +1,8 @@
+<template>
+<svg viewBox="0 0 35 20" height="20px" xmlns="http://www.w3.org/2000/svg"><g shape-rendering="crispEdges"><path d="M0 0h33v20H0z" fill="#555"/></g><g fill="#fff" font-size="110" text-anchor="middle"><text transform="scale(.1)" x="165" y="140" textLength="230">PRO</text></g></svg> 
+</template>
+<script>
+export default {
+  name: 'pro'
+};</script>
+
